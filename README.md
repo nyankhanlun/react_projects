ff8ba8a4d1ef0cc48b4bae3283a3ddce4e47035c
Lun <mac@nklun.local>
2025-10-10 20:17:39 +0630

    add code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# react_projects-
-This is practice Project to be ready for hands-on projects.
+# react_projects
+This is practice Project to be ready for hands-on project
+
+### React + TypeScript 
+1. npm i
+2. npm start
+
+[Note]
+
+* React functional components can use the React.FC generic type to merge custom and built-in props, such as children.
+
+* todoTextInputRef.current?.value  and  todoTextInputRef.current!.value
+    *  The question mark (?.) operator safely accesses properties that might be null, while the exclamation mark (!) asserts that the value is non-null, which should be used cautiously.
+
+* The tsconfig.json file configures the TypeScript compiler for a project.
+* The target option specifies the JavaScript version output, commonly set to es5 for broad browser support.
+* The lib option includes default TypeScript libraries, such as DOM, which enable built-in types like HTMLInputElement.
+* The strict option enforces strict type checking, preventing implicit any types and promoting safer code.
+
+
---
+++ src/store/todo-context.tsx
@@ -0,0 +1,39 @@
+import React, { useState }  from "react";
+import Todo from "../models/todo";
+
+type TodoContextObj = {
+    items: Todo[];
+    addTodo : (text : string) => void;
+    removeTodo : (id : string) => void
+}
+
+export const TodosContext = React.createContext<TodoContextObj>({
+    items : [],
+    addTodo: () => {},
+    removeTodo : (id: string) => {}
+});
+
+const TodosContextProvider : React.FC = (props) => {
+      const [todos, setTodos] =  useState<Todo[]>([])
+  const addTodoHandler = (text : string) => {
+    const newTodo = new Todo(text)
+    setTodos(prevTodo => {
+      return prevTodo.concat(newTodo)
+    })
+  }
+  const removeTodoHandler = (todoId : string) => {
+    setTodos((prevTodo) => {
+      return prevTodo.filter(todo => todo.id !== todoId)
+    })
+  }
+  const contextValue : TodoContextObj = {
+    items : todos,
+    addTodo : addTodoHandler,
+    removeTodo : removeTodoHandler
+  }
+    return <TodosContext.Provider value={contextValue}>
+        {props.children}
+    </TodosContext.Provider>
+}
+
+export default TodosContextProvider;
