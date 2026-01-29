f876b1796777b9495dee717de2c03f16a4d74914
nyankhanlun <nyankhanlun@gmail.com>
2026-01-29 20:57:04 +0630

    remove ignore

--- .next/dev/static/chunks/app_page_tsx_d6e3dabb._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_home_module_54e62cad.css",
-  "static/chunks/node_modules_next_dist_279f65b1._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_songs_page_tsx_d6e3dabb._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/components_Header_652677ed._.css",
-  "static/chunks/node_modules_a0ce85c0._.js",
-  "static/chunks/_88148026._.js"
-],
-    source: "dynamic"
-});
