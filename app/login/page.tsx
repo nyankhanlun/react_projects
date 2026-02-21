6edad2f535c11511a64b0b50514bcc5a7e378a88
nyankhanlun <nyankhanlun@gmail.com>
2026-02-21 15:29:20 +0630

    loggin loading

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,10 +11,13 @@ export default function LoginForm() {
     const [password, setPassword] = useState("");
     const [error, setError] = useState<string | null>(null);
     const [loading, setLoading] = useState(false);
+    const [isLoggedInloading, setIsLoggedInloading] = useState(false);
     const [rememberMe, setRememberMe] = useState<boolean>(false);
+
     const router = useRouter();
 
     useEffect(() => {
+        setIsLoggedInloading(true);
         const unsubscribe = onAuthStateChanged(clientAuth, async (user) => {
             if (!user) {
                 router.push("/login");
@@ -22,6 +25,7 @@ export default function LoginForm() {
                 router.push("/songs");
             }
         });
+        setIsLoggedInloading(false)
         return () => unsubscribe();
     }, [router]);
 
@@ -75,98 +79,100 @@ export default function LoginForm() {
     }
     return (
         <>
-            <section className="bg-gray-50 min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8">
-                <div className="w-full max-w-md sm:max-w-lg md:max-w-xl bg-white rounded-lg shadow-md">
+            {isLoggedInloading ? (
+                <section className="bg-gray-50 min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8">
+                    <div className="w-full max-w-md sm:max-w-lg md:max-w-xl bg-white rounded-lg shadow-md">
 
-                    <div className="p-6 sm:p-8 space-y-4">
+                        <div className="p-6 sm:p-8 space-y-4">
 
-                        <div className="flex flex-col items-center mb-6">
-                            {/* <img
+                            <div className="flex flex-col items-center mb-6">
+                                {/* <img
                                 className="w-10 h-10 mb-2"
                                 src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
                                 alt="logo"
                             /> */}
-                            <h2 className="text-2xl font-semibold text-gray-900">
-                                Worship Library
-                            </h2>
-                        </div>
-
-                        <form className="space-y-4" onSubmit={handleSubmit}>
-
-                            <div>
-                                <label className="block mb-2 text-sm font-medium text-gray-900">
-                                    Email
-                                </label>
-                                <input
-                                    type="email"
-                                    required
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    placeholder="Enter email"
-                                    className="w-full p-3 text-sm bg-gray-50 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                                />
+                                <h2 className="text-2xl font-semibold text-gray-900">
+                                    Worship Library
+                                </h2>
                             </div>
 
-                            <div>
-                                <label className="block mb-2 text-sm font-medium text-gray-900">
-                                    Password
-                                </label>
-                                <input
-                                    type="password"
-                                    required
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    placeholder="Enter password"
-                                    className="w-full p-3 text-sm bg-gray-50 border border-gray-300 rounded-lg 
+                            <form className="space-y-4" onSubmit={handleSubmit}>
+
+                                <div>
+                                    <label className="block mb-2 text-sm font-medium text-gray-900">
+                                        Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        required
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        placeholder="Enter email"
+                                        className="w-full p-3 text-sm bg-gray-50 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                                    />
+                                </div>
+
+                                <div>
+                                    <label className="block mb-2 text-sm font-medium text-gray-900">
+                                        Password
+                                    </label>
+                                    <input
+                                        type="password"
+                                        required
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        placeholder="Enter password"
+                                        className="w-full p-3 text-sm bg-gray-50 border border-gray-300 rounded-lg 
                                     
                                     focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                                />
-                            </div>
-
-                            <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
-                                <label className="flex items-center text-sm text-gray-500">
-                                    <input
-                                        type="checkbox"
-                                        checked={rememberMe}
-                                        onChange={handleCheckboxChange}
-                                        className="mr-2 w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                                     />
-                                    Remember me
-                                </label>
+                                </div>
 
-                                {/* <a onClick={handleForgotPassword}
+                                <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
+                                    <label className="flex items-center text-sm text-gray-500">
+                                        <input
+                                            type="checkbox"
+                                            checked={rememberMe}
+                                            onChange={handleCheckboxChange}
+                                            className="mr-2 w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
+                                        />
+                                        Remember me
+                                    </label>
+
+                                    {/* <a onClick={handleForgotPassword}
                                     className="text-sm font-medium text-blue-600 hover:underline">
                                     Forgot password?
                                 </a> */}
-                            </div>
+                                </div>
 
-                            {error && <p className="text-red-500 text-sm">{error}</p>}
+                                {error && <p className="text-red-500 text-sm">{error}</p>}
 
-                            <button
-                                type="submit"
-                                disabled={loading}
-                                className="w-full py-3 text-sm font-medium text-white bg-blue-600 rounded-lg 
+                                <button
+                                    type="submit"
+                                    disabled={loading}
+                                    className="w-full py-3 text-sm font-medium text-white bg-blue-600 rounded-lg 
                                 disabled:bg-gray-200 
                                 disabled:text-gray-500 
                                 disabled:cursor-not-allowed
                                 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300"
-                            >
-                                {loading ? <p className="animate-bounce">Logging in...</p> : "Log in"}
-                            </button>
+                                >
+                                    {loading ? <p className="animate-bounce">Logging in...</p> : "Log in"}
+                                </button>
 
-                            <p className="text-sm text-gray-500 text-center">
-                                Don’t have an account yet?{" "}
-                                <Link href="/signup" className="font-medium text-blue-600 hover:underline">
-                                    Sign up
-                                </Link>
-                            </p>
+                                <p className="text-sm text-gray-500 text-center">
+                                    Don’t have an account yet?{" "}
+                                    <Link href="/signup" className="font-medium text-blue-600 hover:underline">
+                                        Sign up
+                                    </Link>
+                                </p>
 
-                        </form>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </section>
-
-
+                </section>
+            ) : <div className="flex items-center justify-center h-screen">
+                <p className="animate-bounce">Loading...</p>
+            </div>}
         </>
     )
 }
